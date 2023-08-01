f184d8b4bd12f18c9d61459ea206f61f747ce1da
sammyALX <samuelhailu49@gmail.com>
2023-08-01 14:00:16 +0300

    setup basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Basic flask app
+"""
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
