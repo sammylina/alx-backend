7c18c071e5efc120d9e906028f47f6a5f69c2f5f
sammyALX <samuelhailu49@gmail.com>
2023-08-01 16:31:43 +0300

    enable translation for en and fr

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Basic flask app
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """app configuration constants
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+dir(babel)
+
+
+@app.route('/')
+def home():
+    """root route for the app
+    """
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """choose the best language for user from those we support
+    """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
